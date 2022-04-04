0eaa63fa4736da422d99e879fd5c73c2a4e9202d
Neeraj-admin <neeraj.tiw1988@gmail.com>
2022-04-04 17:25:37 +0530

    pug template engine

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const http=require('http');
 var express = require('express');
 const path = require('path');
+
 var app = express();
+app.set('view engine', 'pug');
+app.set('views','./views');
 const port=process.env.PORT || 3585
 app.get('/', function (req, res) {
   //res.send('Hello World! Testing');
-  res.sendFile(path.join(__dirname+'/home.html'));
+  //res.sendFile(path.join(__dirname+'/home.html'));
+  res.render('home');
 });
 app.get('/contact', function (req, res) {
   //res.send('Hello World! Testing');
-  res.sendFile(path.join(__dirname+'/contact.html'));
+  //res.sendFile(path.join(__dirname+'/contact.html'));
+  res.render('contact');
 });
 app.listen(port, function () {
   console.log('Example app listening on port'+port);
