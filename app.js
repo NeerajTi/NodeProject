5a02cbef10cf4069952be58a7d5e376f43cb1694
Neeraj-admin <neeraj.tiw1988@gmail.com>
2022-04-04 12:15:55 +0530

    Added home.html and update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+const http=require('http');
 var express = require('express');
+const path = require('path');
 var app = express();
 const port=process.env.PORT || 3585
 app.get('/', function (req, res) {
-  res.send('Hello World! Testing');
+  //res.send('Hello World! Testing');
+  res.sendFile(path.join(__dirname+'/home.html'));
 });
 app.listen(port, function () {
   console.log('Example app listening on port'+port);
